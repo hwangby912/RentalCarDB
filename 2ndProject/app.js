502cb2884f8373c608cc123557d42b2e70e3c896
hwangby912 <quddbs912@naver.com>
2019-07-12 01:30:01 +0900

    2019-07-12 yesterday version SPA-2nd-project rentalCar

---
+++ 2ndProject/app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var mysql = require('mysql');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser('2nd project'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  resave: false,
+  saveUninitialized: false, 
+  secret: '2nd project', 
+  cookie: {
+    httpOnly: true,
+    secure: false
+  }
+}));
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+app.use('/signup', require('./routes/signup'));
+app.use('/login', require('./routes/login'));
+
+app.use('/exchange', require('./routes/exchange'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ 2ndProject/public/javascripts/listener.js
@@ -0,0 +1,65 @@
+$(document).ready(() => {
+    $('#signupBtn').click(() => {
+        const userid = $('#signupID').val(); 
+        const userpw = $('#signupPW').val();
+        const name = $('#signupName').val();
+        const phone = $('#signupPhone').val();
+        const email = $('#signupEmail').val();
+
+        if(userid === '') {
+            alert('ID를 입력해주세요.');
+        } else if(userpw === '') {
+            alert('비밀번호를 입력해주세요. ');
+        } else if(name === '') {
+            alert('이름를 입력해주세요. ');
+        } else if(phone === '') {
+            alert('핸드폰 번호를 입력해주세요. ');
+        } else if(email === '') {
+            alert('Email을 입력해주세요. ');
+        } else {
+            sendParams = {
+                userid,
+                userpw,
+                name,
+                phone,
+                email
+            };
+            $.post('/signup', sendParams, (data, status) => {
+                const parsedData = JSON.parse(data);
+                alert('회원 가입이 완료되었습니다. ');
+                $('#signupID').val('');
+                $('#signupPW').val('');
+                $('#signupName').val('');
+                $('#signupPhone').val('');
+                $('#signupEmail').val('');
+            });
+        }
+    });
+
+    $('#loginBtn').click(() => {
+        const userid = $('#loginID').val(); 
+        const userpw = $('#loginPW').val();
+        if(userid === '') {
+            alert('ID를 입력해주세요.');
+        } else if(userpw === '') {
+            alert('비밀번호를 입력해주세요. ');
+        } else {
+            sendParams = {
+                userid,
+                userpw
+            }
+            $.post('/login', sendParams, (data, status) => {
+                try {
+                    $('#loginID').val() = '';
+                    $('#loginPW').val() = '';
+                } catch(err) {
+                    window.location.reload(true);
+                }
+            });
+        }
+    });
+    
+    $('#logoutBtn').click(() => {
+        alert('aaa');
+    });
+});
